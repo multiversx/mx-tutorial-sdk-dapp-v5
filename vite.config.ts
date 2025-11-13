d6fb5e7b1be0d02c1c1df85dbaeb6fe046083040
Tudor Morar <tudor.morar@multiversx.com>
2025-11-13 16:22:48 +0200

    01_create_react_app

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import tailwindcss from '@tailwindcss/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import svgrPlugin from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  server: {
+    port: Number(process.env.PORT) || 3000,
+    strictPort: true,
+    host: true,
+    watch: {
+      usePolling: false,
+      useFsEvents: false
+    },
+    hmr: {
+      overlay: false
+    }
+  },
+  plugins: [
+    react(),
+    basicSsl(),
+    tsconfigPaths(),
+    svgrPlugin(),
+    nodePolyfills({
+      globals: { Buffer: true, global: true, process: true }
+    }),
+    tailwindcss()
+  ],
+  css: {
+    postcss: './postcss.config.js'
+  },
+  build: {
+    outDir: 'build'
+  },
+  preview: {
+    port: 3002,
+    host: 'localhost',
+    strictPort: true
+  }
+});
